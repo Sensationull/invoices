3b056a3929056196aa615dd4cc43e17d8a0a9a35
Andrew Johnson <andrewjohnso17@gmail.com>
2025-03-11 13:24:41 -0700

    initial header for mobile done

--- src/components/InvoiceNavBar.tsx
+++ src/components/InvoiceNavBar.tsx
@@ -1,7 +1,25 @@
 import styles from "./InvoiceNavBar.module.css";
+import MoonIcon from "./MoonIcon";
+import Logo from "./logo";
 
 const InvoiceNavBar = () => {
-  return <div className={styles.container}>InvoiceNavBar</div>;
+  return (
+    <nav className={styles.container}>
+      <span className={styles.iconContainer}>
+        <span className={styles.logoContainer}>
+          <Logo />
+        </span>
+        <MoonIcon />
+      </span>
+      <span className={styles.avatarContainer}>
+        <img
+          src="src/assets/image-avatar.jpg"
+          alt="avatar"
+          className={styles.avatar}
+        />
+      </span>
+    </nav>
+  );
 };
 
 export default InvoiceNavBar;
