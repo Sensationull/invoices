f4f675e4c201991a2796276f4a6eb4492dbffffc
Andrew Johnson <andrewjohnso17@gmail.com>
2025-03-11 12:11:45 -0700

    hooking up InvoiceNavBar

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import styles from "./App.module.css";
+import InvoiceNavBar from "./components/InvoiceNavBar";
 
 function App() {
   return (
-    <>
-      <div className={styles.container}>Invoice header</div>
-    </>
+    <div className={styles.container}>
+      <InvoiceNavBar />
+    </div>
   );
 }
 
---
+++ src/components/InvoiceNavBar.tsx
@@ -0,0 +1,7 @@
+import styles from "./InvoiceNavBar.module.css";
+
+const InvoiceNavBar = () => {
+  return <div className={styles.container}>InvoiceNavBar</div>;
+};
+
+export default InvoiceNavBar;
