27dd66c15ceaaaad210b7d08d833f1a3a367dc87
Andrew Johnson <andrewjohnso17@gmail.com>
2025-03-11 10:48:59 -0700

    initial setup

--- src/App.tsx
+++ src/App.tsx
@@ -6,9 +6,8 @@ import "./App.css";
 function App() {
   const [count, setCount] = useState(0);
 
-  const handleClick = (event: React.BaseSyntheticEvent) => {
-    console.log({ target: event.target.value });
-  };
+  const increment = () => setCount(count + 1);
+
   return (
     <>
       <div>
@@ -19,19 +18,19 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
+
       <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+
+      <div className="header-large">
+        <button onClick={increment}>count is {count}</button>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
       </div>
+
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
-      <button onClick={handleClick}>Some more things</button>
     </>
   );
 }
